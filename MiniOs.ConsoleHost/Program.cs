4b41c9493ea7f8861ddc1a4a142812189e8bf4a0
xcssa <imxcssa@qq.com>
2025-11-09 01:09:11 +0800

    e16

--- MiniOs.ConsoleHost/Program.cs
+++ MiniOs.ConsoleHost/Program.cs
@@ -3,7 +3,7 @@ using MiniOS;
 if (args.Length > 0 && args[0] == "--test")
 {
     var exit = await MiniTests.RunAsync();
-    System.Environment.Exit(exit);
+    Environment.Exit(exit);
 }
 
 await Kernel.BootAsync();
